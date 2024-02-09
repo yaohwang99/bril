a11b14e231902b242ce7deae85dd70eeadd0af12
Yao Hwang <yaohwang99@gmail.com>
2024-02-08 22:37:08 -0800

    Construct dom tree

--- src/dom.h
+++ src/dom.h
@@ -6,69 +6,72 @@
 #include <set>
 #include <unordered_set>
 #include <functional>
+#include <cassert>
+#include <string>
 #include "program.h"
 void dom(Block *head) {
   std::vector<Block *> worklist;
-  std::map<Block *, std::set<Block *>> dom;
-  std::unordered_map<Block *, std::set<Block *>> pred;
   std::unordered_set<Block *> visited;
+  std::vector<std::set<int>> pred;
+  std::unordered_map<Block *, int> block2Idx;
   std::function<void(Block *)> dfs;
   // post order dfs
   dfs = [&](Block *b) {
     for (auto &c : b->child) {
-      pred[c].insert(b);
       if (!visited.count(c)){
         visited.insert(c);
         dfs(c);
       }
     }
-    dom[b].insert(b);
     worklist.push_back(b);
+    pred.push_back(std::set<int>());
+    block2Idx[b] = worklist.size() - 1;
+    assert(pred.size() == worklist.size());
+    for (auto &c : b->child) {
+      pred[block2Idx[c]].insert(worklist.size() - 1);
+    }
   };
   dfs(head);
-  for (int i = worklist.size() - 1; i >= 0; i--) {
-    for (auto &it : dom) {
-      if (it.first == head)
-        continue;
-      it.second.insert(worklist[i]);
+  std::vector<int> idom(worklist.size(), -1);
+  assert(block2Idx[head] == worklist.size() - 1);
+  idom[block2Idx[head]] = block2Idx[head];
+  auto intersect = [&](int finger1, int finger2) -> int {
+    while (finger1 != finger2) {
+      while (finger1 < finger2)
+        finger1 = idom[finger1];
+      while (finger1 > finger2)
+        finger2 = idom[finger2];
     }
-  }
+    return finger1;
+  };
   bool hasChanged = true;
   while(hasChanged) {
     hasChanged = false;
-    for (int i = worklist.size() - 1; i >= 0; i--) {
-      Block *B = worklist[i];
-      int sz = dom[B].size();
-      dom[B].clear();
-      dom[B].insert(B);
-      std::set<Block *> intersect;
-      if (pred[B].size()) {
-        intersect = dom[*pred[B].begin()];
-        auto j = pred[B].begin();
-        ++j;
-        for (; j != pred[B].end(); j++){
-          for(auto it = intersect.begin(); it != intersect.end();){
-            if (!dom[*j].count(*it))
-              it = intersect.erase(it);
-            else
-              it++;
-          }
+    // reversed postorder
+    for (int i = worklist.size() - 2; i >= 0; i--) {
+      int newIDom = -1;
+      for (auto p : pred[i]){
+        if (newIDom == -1 && idom[p] != -1) { // first processed predecessor of i 
+          newIDom = p;
+        } else if (idom[p] != -1 && newIDom != -1) {
+          newIDom = intersect(p, newIDom); 
         }
       }
-      for (Block *b : intersect) {
-        dom[B].insert(b);
+      if (newIDom != idom[i]){
+        idom[i] = newIDom;
+        hasChanged = true;
       }
-
-      hasChanged |= (sz != dom[B].size());
     }
   }
-
-  for(auto it : dom){
-    std::cout<<it.first->name<< ": ";
-    for(Block *b : it.second)
-      std::cout<<b->name<<",";
+  idom[idom.size() - 1] = -1;
+  for (int i = worklist.size() - 1; i >= 0; i--) {
+    std::cout<<worklist[i]->name <<": "<<std::endl; 
+    for (int j = i; idom[j] != -1; j = idom[j]) {
+      std::cout<<worklist[j]->name<<",";
+    }
     std::cout<<std::endl;
   }
+
 
 }
 #endif
